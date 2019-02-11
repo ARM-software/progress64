4974621139e0fc731ebbbe5bdb8205f303d8e309
Ola Liljedahl <ola.liljedahl@arm.com>
2019-02-11 10:51:02 +0100

    rwsync: Enhance example
    Change-Id: Icde4602553e9cbbd582c4899cb589e3a593e7280
    Reviewed-on: https://eu-gerrit-1.euhpc.arm.com/171418
    Tested-by: Ola Liljedahl <ola.liljedahl@arm.com>
    Reviewed-by: Ola Liljedahl <ola.liljedahl@arm.com>

--- examples/rwsync.c
+++ examples/rwsync.c
@@ -3,6 +3,7 @@
 //SPDX-License-Identifier:        BSD-3-Clause
 
 #include <stdio.h>
+#include <string.h>
 #include "p64_rwsync.h"
 #include "expect.h"
 
@@ -10,13 +11,16 @@ int main(void)
 {
     p64_rwsync_t sync;
     p64_rwsync_t s;
-    char data[23];
+    char data[24] = { 0 };
+    data[23] = (char)255;
     p64_rwsync_init(&sync);
     s = p64_rwsync_acquire_rd(&sync);
     EXPECT(p64_rwsync_release_rd(&sync, s) == true);
     s = p64_rwsync_acquire_rd(&sync);
     p64_rwsync_write(&sync, "Mary had a little lamb", data, 23);
     EXPECT(p64_rwsync_release_rd(&sync, s) == false);
+    EXPECT(strncmp(data, "Mary had a little lamb", 23) == 0);
+    EXPECT(data[23] == (char)255);
 
     printf("rwsync tests complete\n");
     return 0;
