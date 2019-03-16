f24b70b1a9dcd6101bef1e4d0d2a5413e913c84f
Ola Liljedahl <ola.liljedahl@arm.com>
2019-03-16 01:33:46 +0100

    rwlock_r: Add missing examples/rwlock_r.c
    Change-Id: I7142af0d3492d02861c1b65228565e3359dabb00
    Reviewed-on: https://eu-gerrit-1.euhpc.arm.com/177356
    Tested-by: Ola Liljedahl <ola.liljedahl@arm.com>
    Reviewed-by: Ola Liljedahl <ola.liljedahl@arm.com>

---
+++ examples/rwlock_r.c
@@ -0,0 +1,32 @@
+//Copyright (c) 2018-2019, ARM Limited. All rights reserved.
+//
+//SPDX-License-Identifier:        BSD-3-Clause
+
+#include <stdio.h>
+#include <string.h>
+#include "p64_rwlock_r.h"
+#include "expect.h"
+
+#define MY_TID 242
+
+int main(void)
+{
+    p64_rwlock_r_t lock_r;
+    p64_rwlock_r_init(&lock_r);
+
+    p64_rwlock_r_acquire_rd(&lock_r, MY_TID);
+    p64_rwlock_r_acquire_rd(&lock_r, MY_TID);
+    p64_rwlock_r_release_rd(&lock_r);
+    p64_rwlock_r_release_rd(&lock_r);
+
+    p64_rwlock_r_acquire_wr(&lock_r, MY_TID);
+    p64_rwlock_r_acquire_wr(&lock_r, MY_TID);
+    p64_rwlock_r_release_wr(&lock_r);
+    p64_rwlock_r_release_wr(&lock_r);
+
+    p64_rwlock_r_acquire_rd(&lock_r, MY_TID);
+    p64_rwlock_r_release_rd(&lock_r);
+
+    printf("rwlock_r tests complete\n");
+    return 0;
+}
