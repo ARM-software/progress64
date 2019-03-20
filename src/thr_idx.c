d1296bdafce7421d2e80a9803f3eae3d7f52e1e1
Ola Liljedahl <ola.liljedahl@arm.com>
2019-03-20 14:11:34 +0100

    Add functions for managing thread indexes
    Change-Id: I631c47fd916fea91db8a4e703010d51f8b5004e1
    Reviewed-on: https://eu-gerrit-1.euhpc.arm.com/177509
    Tested-by: Ola Liljedahl <ola.liljedahl@arm.com>
    Reviewed-by: Ola Liljedahl <ola.liljedahl@arm.com>

---
+++ src/thr_idx.c
@@ -0,0 +1,89 @@
+//Copyright (c) 2019, ARM Limited. All rights reserved.
+//
+//SPDX-License-Identifier:        BSD-3-Clause
+
+#include <assert.h>
+#include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "common.h"
+#include "thr_idx.h"
+#include "build_config.h"
+
+#define NWORDS ((MAXTHREADS + 63) / 64)
+
+static uint64_t thread_words[NWORDS];
+
+static pthread_once_t once = PTHREAD_ONCE_INIT;
+static pthread_key_t key;
+
+static inline intptr_t
+ptr_to_int(void *ptr)
+{
+    assert(ptr != NULL);
+    return (intptr_t)ptr - 1;
+}
+
+
+static inline void *
+int_to_ptr(intptr_t sint)
+{
+    return (void *)(sint + 1);
+}
+
+static void
+destructor(void *ptr)
+{
+    uint32_t idx = ptr_to_int(ptr);
+    __atomic_fetch_and(&thread_words[idx / 64], ~(1U << (idx % 64)), __ATOMIC_RELEASE);
+}
+
+void
+very_first_time(void)
+{
+    if (pthread_key_create(&key, destructor) != 0)
+    {
+	perror("pthread_key_create"), exit(EXIT_FAILURE);
+    }
+}
+
+int32_t
+p64_idx_alloc(void)
+{
+    pthread_once(&once, very_first_time);
+    void *ptr = pthread_getspecific(key);
+    if (ptr != NULL)
+    {
+	return ptr_to_int(ptr);
+    }
+
+    for (uint32_t i = 0; i < NWORDS; i++)
+    {
+	uint64_t word = thread_words[i];
+	while (~word != 0)
+	{
+	    uint32_t bit = __builtin_ctz(~word);
+	    if (64 * i + bit >= MAXTHREADS)
+	    {
+		return -1;
+	    }
+	    if (__atomic_compare_exchange_n(&thread_words[i],
+					    &word,
+					    word | (1U << bit),
+					    /*weak*/0,
+					    __ATOMIC_ACQUIRE,
+					    __ATOMIC_RELAXED))
+	    {
+		int32_t idx = 64 * i + bit;
+		if ((errno = pthread_setspecific(key, int_to_ptr(idx))) != 0)
+		{
+		    perror("pthread_setspecific"), exit(EXIT_FAILURE);
+		}
+		return idx;
+	    }
+	}
+    }
+    return -1;
+}
---
+++ src/thr_idx.h
@@ -0,0 +1,12 @@
+//Copyright (c) 2019, ARM Limited. All rights reserved.
+//
+//SPDX-License-Identifier:        BSD-3-Clause
+
+#ifndef _THR_IDX_H
+#define _THR_IDX_H
+
+#include <stdint.h>
+
+int32_t p64_idx_alloc(void);
+
+#endif
