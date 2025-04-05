93577c550af89ac2e2488b5c038e3853af6b4c49
Ola Liljedahl <ola.liljedahl@arm.com>
2025-04-05 20:56:01 +0000

    verify: Add missing lockaba verification module
    Change-Id: I9b23aa82bf97372afa250e2a5417a4ee43232f9c
    Reviewed-on: https://eu-gerrit-1.euhpc.arm.com/c/attk/progress64/+/746488
    Tested-by: Ola Liljedahl <ola.liljedahl@arm.com>
    Reviewed-by: Ola Liljedahl <ola.liljedahl@arm.com>

--- src/verify.c
+++ src/verify.c
@@ -114,6 +114,7 @@ extern struct ver_funcs ver_cuckooht1, ver_cuckooht2;
 extern struct ver_funcs ver_ringbuf_mpmc, ver_ringbuf_nbenbd, ver_ringbuf_nbelfd, ver_ringbuf_spsc;
 extern struct ver_funcs ver_hopscotch1;
 extern struct ver_funcs ver_linklist1, ver_linklist2, ver_linklist3, ver_linklist4;
+extern struct ver_funcs ver_lockaba;
 
 //Table of registered verification modules
 static struct ver_funcs *ver_table[] =
@@ -135,6 +136,7 @@ static struct ver_funcs *ver_table[] =
     &ver_ringbuf_mpmc, &ver_ringbuf_nbenbd, &ver_ringbuf_nbelfd, &ver_ringbuf_spsc,
     &ver_hopscotch1,
     &ver_linklist1, &ver_linklist2, &ver_linklist3, &ver_linklist4,
+    &ver_lockaba,
     NULL
 };
 
